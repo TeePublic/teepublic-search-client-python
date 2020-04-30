6b1100d7753671f79ca9d3f6da26ecdb8addcc82
Jimmy Zhang <jimmy@teepublic.com>
2020-04-30 14:06:03 -0400

    Update search relevancy endpoints.

--- swagger_client/__init__.py
+++ swagger_client/__init__.py
@@ -16,9 +16,9 @@
 from __future__ import absolute_import
 
 # import apis into sdk package
-from swagger_client.api.admin_api import AdminApi
 from swagger_client.api.autocomplete_api import AutocompleteApi
 from swagger_client.api.lookup_api import LookupApi
+from swagger_client.api.relevancy_api import RelevancyApi
 from swagger_client.api.search_api import SearchApi
 
 # import ApiClient
--- swagger_client/api/__init__.py
+++ swagger_client/api/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.admin_api import AdminApi
 from swagger_client.api.autocomplete_api import AutocompleteApi
 from swagger_client.api.lookup_api import LookupApi
+from swagger_client.api.relevancy_api import RelevancyApi
 from swagger_client.api.search_api import SearchApi
